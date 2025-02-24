a60a5da43d616d02143e67fa9a53c812a94bcf69
MohammedZweiri <mohammedzweiri@hotmail.com>
2025-02-24 14:28:55 +0000

    feat: main.py file updated to load, split and normalise the data

---
+++ main.py
@@ -0,0 +1,54 @@
+""" main.py is a centralised module where the process for both tasks initiates
+
+
+"""
+
+from src import utils
+import argparse
+import numpy as np
+
+
+def task(image_path):
+    """ Runs the CNN model for bloodMNIST dataset
+
+
+    """
+
+    print("################ Task B via CNN is starting ################")
+    print('\n')
+
+    # Download the dataset
+    x_train, y_train, x_val, y_val, x_test, y_test = utils.load_dataset(image_path)
+
+    # Transform data using normalization
+    normalised_train, normalised_val, normalised_test = utils.normalize_dataset(x_train, x_val, x_test)
+
+    # visualise a subset of the dataset
+    utils.visualise_subset(normalised_train, y_train)
+
+    # # Run the CNN model
+
+    # if decision == 'train':
+    #     CNN_B.CNN_model_training(train_dataset, validation_dataset, test_dataset)
+        
+    # elif decision == 'test':
+    #     CNN_B.CNN_model_testing(test_dataset)
+
+    # print('\n')
+    # print("################ Task B via CNN has finished ################")
+
+
+if __name__ == "__main__":
+    # Create Datasets folder
+    utils.create_directory("figures")
+    dataset_path = "./Dataset/"
+    task(dataset_path)
+
+
+    
+
+
+
+
+
+
