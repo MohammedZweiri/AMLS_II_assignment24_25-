8e0781ac60f40732b666f6f3463ca3c8eddbb511
MohammedZweiri <mohammedzweiri@hotmail.com>
2025-03-16 22:53:47 +0000

    feat: utils.py and efficientNet.py files updated to tune hyper-parameters

--- model/efficientNet.py
+++ model/efficientNet.py
@@ -180,7 +180,7 @@ def EfficientNet_model_training(train_dataset, train_labels, val_dataset, val_la
         model.add(Dropout(0.3))
         model.add(Dense(128, activation="relu"))
         model.add(BatchNormalization())
-        model.add(Dropout(0.3))
+        model.add(Dropout(0.5))
         #model.add(Dropout(0.5))
         model.add(Dense(5, activation="softmax"))
         # Output the model summary
@@ -198,7 +198,7 @@ def EfficientNet_model_training(train_dataset, train_labels, val_dataset, val_la
 
         # Plot the CNN model
         plot_model(model, 
-                to_file='./figures/EfficientNet_Model_test_17.png', 
+                to_file='./figures/EfficientNet_Model_test_18.png', 
                 show_shapes=True,
                 show_layer_activations=True)
 
@@ -216,12 +216,12 @@ def EfficientNet_model_training(train_dataset, train_labels, val_dataset, val_la
                 epochs=30,
                 callbacks=[tf.keras.callbacks.EarlyStopping(patience=8)],
                 validation_data=(val_dataset, val_labels_categorical),
-                batch_size=32,
+                batch_size=16,
                 shuffle=True,
                 class_weight=weights)
         
         # save the CNN model
-        utils.save_model(model, "EfficientNet_Model_test_add_17")
+        utils.save_model(model, "EfficientNet_Model_test_add_18")
 
         utils.plot_accuray_loss(history)
 
