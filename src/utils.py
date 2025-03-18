2103cc072a04fb0eb45311e4db3f3b7dbd4035cc
MohammedZweiri <mohammedzweiri@hotmail.com>
2025-03-18 13:58:16 +0000

    feat: utils.py and efficientNet.py update to change reizing images from 300x300 to 224x224 as it is a required resolution for EfficientNetB0 model

--- src/utils.py
+++ src/utils.py
@@ -55,7 +55,7 @@ def load_dataset(image_path):
     """
     try:
 
-        TARGET_SIZE = 300
+        TARGET_SIZE = 224
 
         train = pd.read_csv(image_path+'/train.csv')
 
@@ -220,7 +220,7 @@ def plot_accuray_loss(model_history):
         ax2.grid()
 
         # Save the subplots file.`125   q`
-        fig.savefig(f'./figures/Efficient_accuracy_loss_21.png')
+        fig.savefig(f'./figures/Efficient_accuracy_loss_22.png')
     
     except Exception as e:
         print(f"Plotting accuracy and loss has failed. Error: {e}")
